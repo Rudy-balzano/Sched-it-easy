7d032eb5f89feacf0585a8f66db59139ef1c8e97
rudy <rudybalzano16@gmail.com>
2020-12-02 18:32:49 +0100

    FindByUsername

--- src/persist/MySQLUserDAO.java
+++ src/persist/MySQLUserDAO.java
@@ -1,4 +1,31 @@
 package persist;
 
-public class MySQLUserDAO {
+import java.util.ArrayList;
+import java.sql.*;
+
+public class MySQLUserDAO implements UserDAO {
+
+    ConnectionDBMySQL instanceConnection = ConnectionDBMySQL.getInstance();
+    Connection connection = instanceConnection.getConnection();
+
+    public ArrayList<String> findByUsername(String username){
+
+        ArrayList<String> user = new ArrayList<>();
+
+        try{
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery("select * from users where username = '" + username + "';");
+            if(rs.next()){
+                user.add(rs.getString(2));
+                user.add(rs.getString(3));
+                user.add(rs.getString(4));
+                user.add(rs.getString(5));
+            }
+        } catch (SQLException ex){
+            System.out.println("SQL request error");
+        }
+
+        return user;
+    }
+
 }
