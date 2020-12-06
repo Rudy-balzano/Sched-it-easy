aff78727435cb9921377c41897c9baf59b4b10f1
pierreperrin <perr1.pierre1@gmail.com>
2020-12-06 16:50:57 +0100

    implementation handleregister

--- src/gui/controllers/RegisterController.java
+++ src/gui/controllers/RegisterController.java
@@ -1,15 +1,18 @@
 package gui.controllers;
 
+import core.SessionFacade;
 import gui.Main;
 import gui.roots.Roots;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Window;
 
-import java.awt.event.ActionEvent;
 import java.io.IOException;
 
 public class RegisterController {
@@ -40,5 +43,36 @@ public class RegisterController {
         Parent loginRoot = FXMLLoader.load(getClass().getResource(Roots.loginRoot));
         Scene loginView = new Scene(loginRoot);
         Main.scheditWindow.setScene(loginView);
+    }
+
+
+    public void handleRegister(ActionEvent actionEvent) {
+        SessionFacade session = new SessionFacade();
+
+        String Username = username.getText();
+        String Firstname = firstname.getText();
+        String Lastname = lastname.getText();
+        String Password = password.getText();
+        String ConfirmPassword = confirmPassword.getText();
+
+        if (Password != ConfirmPassword){
+            System.out.println("password and confirmPassword are different !");
+            password.setText("");
+            confirmPassword.setText("");
+        }
+        else {
+            Boolean register = false;
+
+            register = session.register(Username, Firstname, Lastname, Password);
+
+            if (register){
+                System.out.println("Waiting to be accept by the administrator");
+            }
+            else {
+                System.out.println("Username already exist !");
+            }
+        }
+
+
     }
 }
