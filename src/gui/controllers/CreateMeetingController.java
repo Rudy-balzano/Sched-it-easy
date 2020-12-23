a012e93caeaa80c798d65536a203173716471769
pierreperrin <perr1.pierre1@gmail.com>
2020-12-23 20:12:47 +0100

    createmeeting view

--- src/gui/controllers/CreateMeetingController.java
+++ src/gui/controllers/CreateMeetingController.java
@@ -1,6 +1,40 @@
 package gui.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
 public class CreateMeetingController {
 
 
+    @FXML
+    private TextField topic;
+
+    @FXML
+    private DatePicker date;
+
+    @FXML
+    private ChoiceBox hour_meeting;
+
+    @FXML
+    private ChoiceBox minute_meeting;
+
+    @FXML
+    private ChoiceBox hour_duration;
+
+    @FXML
+    private ChoiceBox minute_duration;
+
+
+    public void handleCreateMeeting(ActionEvent actionEvent) {
+    }
+
+    public void handleCancel(ActionEvent actionEvent) {
+    }
+
+    public void handleBookRoom(ActionEvent actionEvent) {
+    }
 }
