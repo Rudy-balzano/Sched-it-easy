ea44956dc81e553bdf0ea4e609fb52b5e7cfe7a7
rudy <rudybalzano16@gmail.com>
2020-12-23 18:46:07 +0100

    ReservationFacade

---
+++ src/core/ReservationFacade.java
@@ -0,0 +1,31 @@
+package core;
+
+import persist.FactoryDAOImpl;
+import persist.MeetingDAO;
+import persist.InvitationDAO;
+import core.SessionFacade;
+
+import java.util.Date;
+
+public class ReservationFacade {
+
+    private User connectedUser;
+    private FactoryDAOImpl factoryDAO;
+    private MeetingDAO meetingDAO;
+
+    /**
+     * Default constructor
+     */
+    public ReservationFacade() {
+        this.factoryDAO = FactoryDAOImpl.getInstance();
+        this.meetingDAO = factoryDAO.createMeetingDAO();
+    }
+
+    public boolean createMeeting(String topic, Date date,int duree){
+        User creator = SessionFacade.getConnectedUser();
+        String creatorUsername = creator.getUserName();
+        return meetingDAO.insert(topic,date,duree,creatorUsername);
+    }
+
+
+}
