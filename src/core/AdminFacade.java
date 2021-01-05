de68fcd792b3c501257047177a587aef032e26c0
Emiliejean <jean.emilie34@hotmail.fr>
2021-01-05 11:13:59 +0100

    admin changes

--- src/core/AdminFacade.java
+++ src/core/AdminFacade.java
@@ -12,16 +12,16 @@ public class AdminFacade {
         this.adminDAO = factoryDAO.createAdminDAO();
     }
 
-    public void changeAccount(String username, String lastName, String firstName){
+    public boolean changeAccount(String lastName, String firstName){
         Admin old = adminDAO.findByUsername(SessionFacade.getConnectedAdmin().getUsername());
         Admin nw = new Admin();
 
-        if(!username.isBlank()){nw.setUsername(username);}else{nw.setUsername(old.getUsername());}
+        nw.setUsername(old.getUsername());
         if(!firstName.isBlank()){nw.setFirstName(firstName);}else{nw.setFirstName(old.getFirstName());}
         if(!lastName.isBlank()){nw.setLastName(lastName);}else{nw.setLastName(old.getLastName());}
         nw.setPassword(old.getPassword());
 
         adminDAO.deleteAdmin(old.getUsername());
-        adminDAO.insertAdmin(nw.getUsername(), nw.getFirstName(), nw.getLastName(), nw.getPassword());
+        return adminDAO.insertAdmin(nw.getUsername(), nw.getFirstName(), nw.getLastName(), nw.getPassword());
     }
 }
--- src/gui/controllers/AdminSeeAccountController.java
+++ src/gui/controllers/AdminSeeAccountController.java
@@ -2,15 +2,14 @@ package gui.controllers;
 
 import core.AdminFacade;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextField;
 
 
-public class AdminSeeAccountController {
+public class AdminSeeAccountController implements AlertShower{
 
     private final AdminFacade facade = new AdminFacade();
 
-    @FXML
-    private TextField username;
     @FXML
     private TextField firstname;
     @FXML
@@ -18,6 +17,12 @@ public class AdminSeeAccountController {
 
     @FXML
     public void submitChanges(){
-        facade.changeAccount(username.getText(),lastname.getText(),firstname.getText());
+        boolean res = facade.changeAccount(lastname.getText(),firstname.getText());
+        if(res) {
+            showAlert(Alert.AlertType.CONFIRMATION, firstname.getScene().getWindow(), "Success!", "Profile updated successfully!");
+        }else{
+            showAlert(Alert.AlertType.ERROR, firstname.getScene().getWindow(), "Failed!", "Failed to update profile...");
+        }
+
     }
 }
