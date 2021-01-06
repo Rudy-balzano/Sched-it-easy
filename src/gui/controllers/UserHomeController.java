70f4391c0e5a236b72fd1574abb8f8b1fe091edf
baptiste-mouton <baptiste.mouton@etu.umontpellier.fr>
2021-01-06 17:48:55 +0100

    User's home page readonly

--- src/gui/controllers/UserHomeController.java
+++ src/gui/controllers/UserHomeController.java
@@ -4,7 +4,6 @@ import com.calendarfx.model.Calendar;
 import com.calendarfx.model.CalendarEvent;
 import com.calendarfx.model.CalendarSource;
 import com.calendarfx.model.Entry;
-import com.calendarfx.view.WeekView;
 import com.calendarfx.view.page.WeekPage;
 import core.Meeting;
 import core.ScheduleFacade;
@@ -12,8 +11,6 @@ import core.SessionFacade;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 
-import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.Collection;
 
 
@@ -22,29 +19,17 @@ public class UserHomeController {
     @FXML
     private WeekPage weekPage;
 
-    private Calendar calendar = new Calendar("Calendar");
-    private Collection<Entry> entries;
-    //user = getUsername;
-   //listEntries = facade.checkSchedule(user);
+    Calendar calendar = new Calendar("Calendar");
 
     private static ScheduleFacade facade = new ScheduleFacade();
-    private static SessionFacade sfacade = new SessionFacade();
-
-    private Collection<Meeting> meetings;
-
-    private void handle(CalendarEvent calendarEvent){
-        if (calendarEvent.isEntryAdded()){
-            entries.add(calendarEvent.getEntry());
-        }
-        else if (calendarEvent.isEntryRemoved()){
-            entries.remove(calendarEvent.getEntry());
-        }
-    }
 
     @FXML
     private void initialize(){
+        //Setting the ui readonly
         calendar.setReadOnly(true);
-        EventHandler<CalendarEvent> handler = calendarEvent -> handle(calendarEvent);
+        weekPage.setContextMenuCallback(null);
+        weekPage.setEntryFactory(param -> null);
+
         calendar.setStyle(Calendar.Style.STYLE1);
 
         Collection<Meeting> meetings = facade.checkSchedule(SessionFacade.getConnectedUser().getUserName());
@@ -65,6 +50,7 @@ public class UserHomeController {
         CalendarSource calendarSource = new CalendarSource("Week calendar");
         calendarSource.getCalendars().addAll(calendar);
         weekPage.getCalendarSources().addAll(calendarSource);
+
 
     }
 
