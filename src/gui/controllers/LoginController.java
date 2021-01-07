8fe1692cccc62e331a45715735b592aedf50cfd1
Emiliejean <jean.emilie34@hotmail.fr>
2021-01-07 19:18:13 +0100

    test unitaire JUnit login

--- src/gui/controllers/LoginController.java
+++ src/gui/controllers/LoginController.java
@@ -11,6 +11,7 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.stage.Window;
+import org.junit.Test;
 
 import java.io.IOException;
 
---
+++ src/test/LoginTest.java
@@ -0,0 +1,42 @@
+package test;
+import core.SessionFacade;
+import core.User;
+import junit.framework.*;
+import org.junit.Test;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+public class LoginTest extends TestCase {
+
+    SessionFacade sf;
+
+    @BeforeEach
+    public void initSession() {
+       System.out.println("Appel avant chaque test");
+       sf = new SessionFacade();
+
+    }
+
+    @Test
+    public void testLogin() {
+        //Arrange
+        String username = "manager";
+        String password = "mdp";
+        SessionFacade sf = new SessionFacade();
+        int connected = sf.login(username, password);
+        assertEquals(0, connected);
+    }
+
+    @AfterEach
+    public void undoSession() {
+        System.out.println("Appel après chaque test");
+        sf = null;
+    }
+
+    public static void main(String args[]) {
+        junit.textui.TestRunner.run(new TestSuite(LoginTest.class));
+    }
+}
+
+
+
